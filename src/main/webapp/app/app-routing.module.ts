df20f9703bf0e768ac2993d85eaf92881df4e4de
Jazmin Arias <jariass@ucenfotec.ac.cr>
2021-07-09 00:14:29 -0600

    Login
    login done, need to fix the login sidebar

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 
 import { errorRoute } from './layouts/error/error.route';
 import { navbarRoute } from './layouts/navbar/navbar.route';
+import { LOGIN_ROUTE } from './login/login.route'
 import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 import { Authority } from 'app/config/authority.constants';
 
