681fd3e19b228dcf05ee4da6aeca1eafb8e683c0
shaneyTEC <63270555+shaneyTEC@users.noreply.github.com>
2021-07-11 19:57:08 -0600

    General information display
    - Personal information display.
    - Doctors specialty description display.
    - Show information, depending on user rol.

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -5,8 +5,6 @@ import { takeUntil } from 'rxjs/operators';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
-import { Doctor } from 'app/entities/doctor/doctor.model';
-import { Patient } from "../entities/patient/patient.model";
 
 @Component({
   selector: 'medi-home',
@@ -15,22 +13,17 @@ import { Patient } from "../entities/patient/patient.model";
 })
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
-  doctordetails: Doctor | null = null;
-  patientdetails: Patient | null = null;
-  patientdetails: Patient | null = null;
 
   private readonly destroy$ = new Subject<void>();
 
-  constructor(private accountService: AccountService, private router: Router, private doctorDetails: Doctor,
-              private patientDetails: Patient) {}
+  constructor(private accountService: AccountService,
+              private router: Router) {}
 
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => (this.account = account));
-    this.doctordetails = this.doctorDetails;
-    this.patientdetails = this.patientDetails;
   }
 
   login(): void {
