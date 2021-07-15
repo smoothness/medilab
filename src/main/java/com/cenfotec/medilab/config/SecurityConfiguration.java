5d83095b724c77c61bef552d63affefa9680947d
Jazmin Arias <jariass@ucenfotec.ac.cr>
2021-07-14 23:42:12 -0600

    Update SecurityConfiguration.java

--- src/main/java/com/cenfotec/medilab/config/SecurityConfiguration.java
+++ src/main/java/com/cenfotec/medilab/config/SecurityConfiguration.java
@@ -80,6 +80,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
+            .csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .and()
             .addFilterBefore(corsFilter, CsrfFilter.class)
             .exceptionHandling()
                 .authenticationEntryPoint(problemSupport)
