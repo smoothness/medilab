1b99929eea9af78ec440f41b7766eccd9dd0065e
jazmin0226 <jariass@ucenfotec.ac.cr>
2021-07-22 01:19:16 -0600

    Fix to recover password

--- src/main/webapp/app/account/password-reset/init/password-reset-init.component.ts
+++ src/main/webapp/app/account/password-reset/init/password-reset-init.component.ts
@@ -1,11 +1,15 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Router } from "@angular/router";
 import { FormBuilder, Validators } from '@angular/forms';
 
 import { PasswordResetInitService } from './password-reset-init.service';
+import { SweetAlertServiceService } from "../../../shared/services/sweet-alert-service.service";
+
 
 @Component({
   selector: 'medi-password-reset-init',
   templateUrl: './password-reset-init.component.html',
+  styleUrls: ["./password-reset-init.component.scss"]
 })
 export class PasswordResetInitComponent implements AfterViewInit {
   @ViewChild('email', { static: false })
@@ -16,15 +20,30 @@ export class PasswordResetInitComponent implements AfterViewInit {
     email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
   });
 
-  constructor(private passwordResetInitService: PasswordResetInitService, private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private passwordResetInitService: PasswordResetInitService,
+    private sweetAlertService: SweetAlertServiceService,
+  ) {}
 
-  ngAfterViewInit(): void {
+  public get emailRecover(): string {
+    return <string>this.resetRequestForm.get(['email'])!.value
+  }
+
+  public ngAfterViewInit(): void {
     if (this.email) {
       this.email.nativeElement.focus();
     }
   }
 
-  requestReset(): void {
-    this.passwordResetInitService.save(this.resetRequestForm.get(['email'])!.value).subscribe(() => (this.success = true));
+  public requestReset(): void {
+    this.passwordResetInitService.save(this.emailRecover).subscribe(() => {
+      this.sweetAlertService.showMsjSuccess('reset.request.messages.success', 'reset.done').then(() => {
+        this.resetRequestForm.reset();
+        this.router.navigate(['/']);
+      })
+      this.success = true
+    });
   }
 }
