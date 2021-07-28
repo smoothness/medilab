03475cf267f919ccb1834537a1afae60dde62361
jazmin0226 <jariass@ucenfotec.ac.cr>
2021-07-28 14:55:12 -0600

    change the invoice variable from type IInvoice to any

--- src/main/webapp/app/entities/invoice/detail/invoice-detail.component.ts
+++ src/main/webapp/app/entities/invoice/detail/invoice-detail.component.ts
@@ -1,23 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { AppointmentService } from 'app/entities/appointment/service/appointment.service';
 import { PatientService } from 'app/entities/patient/service/patient.service';
-
-import { IInvoice } from '../invoice.model';
 import { InvoiceService } from '../service/invoice.service';
-
-
 
 @Component({
   selector: 'medi-invoice-detail',
   templateUrl: './invoice-detail.component.html',
 })
 export class InvoiceDetailComponent implements OnInit {
-  invoice: IInvoice | null = null;
+  invoice: any | null = null;
   patient: any | null = null;
 
   constructor(
-    protected invoiceServie : InvoiceService,
+    protected invoiceService : InvoiceService,
     protected appointmentService : AppointmentService,
     protected patientService : PatientService,
     protected activatedRoute: ActivatedRoute) {}
@@ -25,10 +22,10 @@ export class InvoiceDetailComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ invoice }) => {
       this.invoice = invoice;
-        
-      this.patientService.findOneByAppointmen(<number>this.invoice?.appointment?.id).subscribe((data) => {
+
+      this.patientService.findOneByAppointmen(<number>this.invoice?.appointment?.id).subscribe((data: any) => {
         this.patient = data.body;
-      })       
+      })
     });
   }
 
