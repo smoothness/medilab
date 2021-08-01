9c5f66fa76839c4523d4d6767681d75b3f2a4c4d
Josué B <jbrenesr1@ucenfotec.ac.cr>
2021-08-01 02:27:37 -0600

    TS de ailment-report.component

---
+++ src/main/webapp/app/entities/ailment/report/ailment-report.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { IAilment } from '../ailment.model';
+import { AilmentService } from '../service/ailment.service';
+
+@Component({
+  selector: 'medi-ailment-report',
+  templateUrl: './ailment-report.component.html',
+  styleUrls: ['./ailment-report.component.scss'],
+})
+export class AilmentReportComponent implements OnInit {
+  public ailments: IAilment[] = [];
+
+  constructor(protected ailmentService: AilmentService) {}
+
+  ngOnInit(): void {
+    this.prueba();
+  }
+
+  prueba(): void {
+    this.getAilmentReport();
+  }
+
+  protected getAilmentReport(): void {
+    this.ailmentService.getAilmentReport().subscribe((ailmentResponse: any) => {
+      this.ailments = ailmentResponse.body;
+    });
+  }
+}
