37db940ffdd4afff613f13115e7aed39eb9e79e3
jazmin0226 <jariass@ucenfotec.ac.cr>
2021-08-19 11:39:13 -0600

    crear back button

--- src/main/webapp/app/shared/back-button/back-button.component.ts
+++ src/main/webapp/app/shared/back-button/back-button.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'medi-back-button',
   templateUrl: './back-button.component.html',
   styleUrls: ['./back-button.component.scss']
 })
-export class BackButtonComponent implements OnInit {
+export class BackButtonComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  previousState(): void {
+    window.history.back();
   }
-
 }
