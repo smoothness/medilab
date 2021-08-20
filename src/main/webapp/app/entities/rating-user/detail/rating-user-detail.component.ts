9e73dafbf52e9a57088962f59dea25dbee091639
jazmin0226 <jariass@ucenfotec.ac.cr>
2021-08-19 20:41:36 -0600

    Add btn view doctor profile on doctors list

--- src/main/webapp/app/entities/rating-user/detail/rating-user-detail.component.ts
+++ src/main/webapp/app/entities/rating-user/detail/rating-user-detail.component.ts
@@ -5,24 +5,7 @@ import { Doctor } from "../../../core/auth/account.model";
 
 @Component({
   selector: 'medi-rating-user-detail',
-  templateUrl: './rating-user-detail.component.html',
-  styles: [`
-    .star {
-      position: relative;
-      display: inline-block;
-      font-size: 3rem;
-      color: #d3d3d3;
-    }
-    .full {
-      color: red;
-    }
-    .half {
-      position: absolute;
-      display: inline-block;
-      overflow: hidden;
-      color: red;
-    }
-  `]
+  templateUrl: './rating-user-detail.component.html'
 })
 export class RatingUserDetailComponent implements OnInit {
   @Input() doctor: any = {};
